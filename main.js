1859b1c28754cc263bc1c8db5b2119850b133cd4
Sherpa <alex@sherpa.one>
2022-01-11 21:12:46 +0100

    up

---
+++ main.js
@@ -0,0 +1,27 @@
+/*
+- Ecrire un programme qui retourne les entiers de 1 à 100
+- Remplacer les multiples de 3 par "Fizz"
+- Remplacer les multiples de 5 par "Buzz"
+- Remplacer les multiples de 15 par "BuzzFizz"
+*/
+
+const Fizzbuzz = require("./Fizzbuzz");
+
+let fb = new Fizzbuzz();
+
+let index = 1;
+
+while (index < 101) {
+
+    if (fb.buzzfizz(index)) {
+        console.log("BuzzFizz");
+    } else if (fb.buzz(index)) {
+        console.log("Buzz");
+    } else if (fb.fizz(index)) {
+        console.log("Fizz");
+    } else {
+        console.log(index);
+    }
+
+    index++;
+}
